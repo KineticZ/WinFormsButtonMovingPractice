ff564890eaa13a93dd5da7433ea792acad533a23
Zanlin Oo <zanlin87@gmail.com>
2020-09-16 09:23:10 -0700

    Add project files.

---
+++ WinFormsPractice/Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace WinFormsPractice
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = global::WinFormsPractice.Properties.Resources.ZLO00511;
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1030, 630);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseEnter += new System.EventHandler(this.pictureBox1_MouseEnter);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            // 
+            // imageList1
+            // 
+            this.imageList1.ColorDepth = System.Windows.Forms.ColorDepth.Depth8Bit;
+            this.imageList1.ImageSize = new System.Drawing.Size(16, 16);
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(290, 405);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(271, 110);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(50, 405);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(203, 135);
+            this.pictureBox2.TabIndex = 2;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox2_MouseMove);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Location = new System.Drawing.Point(358, 248);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(203, 135);
+            this.pictureBox3.TabIndex = 3;
+            this.pictureBox3.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1030, 630);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.MouseEnter += new System.EventHandler(this.Form1_MouseEnter);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ WinFormsPractice/Form1.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices.ComTypes;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormsPractice
+{
+    public partial class Form1 : Form
+    {
+        bool isWindowNormal { get; set; }
+
+        private Point MousePoint { get; set; }
+        public Form1()
+        {
+            InitializeComponent();
+            isWindowNormal = false;
+            //WindowState = FormWindowState.Maximized; 
+            pictureBox1.BackgroundImageLayout = ImageLayout.Stretch;
+            pictureBox3.Location = new Point(MousePosition.X, MousePosition.Y);
+
+            pictureBox2.BackColor = Color.FromArgb(0, 100, 100);
+            pictureBox3.BackColor = Color.FromArgb(100, 0, 10);
+
+            pictureBox3.Width = 10;
+            pictureBox3.Height = 10;
+        }
+        private void MoveButton()
+        {
+            int speed = 20;
+            int offset = 100;
+            Rectangle rect = button1.ClientRectangle;
+            int x = button1.Location.X;
+            int y = button1.Location.Y;
+            int width = rect.Width;
+            int height = rect.Height;
+
+            bool isUnderTheTop = MousePoint.Y > y;
+            bool isAboveBottom = MousePoint.Y < y + height;
+            bool isLeftOfTheLeft = MousePoint.X > x - offset && MousePoint.X < x + width / 2f;
+            bool isRightOfTheRight = MousePoint.X < (x + width) + offset && MousePoint.X > x + width / 2f;
+            if (isLeftOfTheLeft && isUnderTheTop && isAboveBottom)
+            {
+                button1.Location = new Point(button1.Location.X + speed, button1.Location.Y);
+            }
+            else if (isRightOfTheRight && isUnderTheTop && isAboveBottom)
+            {
+                button1.Location = new Point(button1.Location.X - speed, button1.Location.Y);
+            }
+
+            bool isRightOfLeft = MousePoint.X > x;
+            bool isLeftOfRight = MousePoint.X < x + width;
+            bool isTopOfTop = MousePoint.Y > y - offset && MousePoint.Y < y + height / 2f;
+            bool isBottomOfBottom = MousePoint.Y < (y + height) + offset && MousePoint.Y > y + height / 2f;
+            if (isTopOfTop && isRightOfLeft && isLeftOfRight)
+            {
+                button1.Location = new Point(button1.Location.X, button1.Location.Y + speed);
+            }
+            else if(isBottomOfBottom && isRightOfLeft && isLeftOfRight)
+            {
+                button1.Location = new Point(button1.Location.X, button1.Location.Y - speed);
+            }
+
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (isWindowNormal)
+            {
+                isWindowNormal = false;
+                WindowState = FormWindowState.Maximized;
+            }
+            else
+            {
+                isWindowNormal = true;
+                WindowState = FormWindowState.Normal;
+            }
+ 
+        }
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            MousePoint = new Point(e.Location.X, e.Location.Y);
+
+            pictureBox2.Visible = true;
+            int offset = 40;
+            Rectangle rect = button1.ClientRectangle;
+            int x = button1.Location.X;
+            int y = button1.Location.Y;
+            int width = rect.Width;
+            int height = rect.Height;
+
+            pictureBox2.Height = height - (2*offset);
+            pictureBox2.Width = width - (2*offset);
+            pictureBox2.Location = new Point(x + offset, y + offset);
+
+            pictureBox3.Location = new Point(e.Location.X, e.Location.Y);          
+            
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {            
+            
+        }
+
+        private void pictureBox2_MouseMove(object sender, MouseEventArgs e)
+        {
+            pictureBox2.Visible = false;
+        }
+
+        private void pictureBox1_MouseEnter(object sender, EventArgs e)
+        {            
+            MoveButton();           
+        }
+        
+        private void Form1_MouseEnter(object sender, EventArgs e)
+        {
+            MoveButton();
+        }
+    }
+}
